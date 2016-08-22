f64ee8b2f9a123f2e83b54322ca4b4e6788df412
biezhihua <448326497@qq.com>
2016-08-22 23:24:53 +0800

    update

--- src/com/bzh/Main.java
+++ src/com/bzh/Main.java
@@ -1,28 +1,73 @@
 package com.bzh;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-
 public class Main {
 
+
+    static Object lock1 = new Object();
+    static Object lock2 = new Object();
+
+
     public static void main(String[] args) {
-        Random random = new Random(47);
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < 15; i++) {
-            int key = random.nextInt(Integer.MAX_VALUE);
-            map.put(key, key);
-            map.put(key, key);
-            System.out.println(Integer.toBinaryString(hash(key)));
+        Bussiness bussiness = new Bussiness();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bussiness.MainThread();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bussiness.SubThread();
+            }
+        }).start();
+
+    }
+
+
+    static class Bussiness {
+        private Object LOCK = new Object();
+        volatile boolean bShouldSub = true;//这里相当于定义了控制该谁执行的一个信号灯
+
+        public void MainThread() {
+            while (true)
+                synchronized (LOCK) {//notify和wait的对象一定要和synchronized的对象保持一致
+                    for (int j = 0; j < 5; j++) {
+                        System.out.println(Thread.currentThread().getName() + "MainThread j=" + j);
+                    }
+                    if (bShouldSub) {
+                        bShouldSub = false;
+                        LOCK.notify();
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generatedcatch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
         }
 
 
+        public void SubThread() {
+            while (true)
+                synchronized (LOCK) {
+                    for (int j = 0; j < 5; j++) {
+                        System.out.println(Thread.currentThread().getName() + "+SubThread:j=" + j);
+                    }
+                    if (!bShouldSub) {
+                        bShouldSub = true;
+                        LOCK.notify();
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generatedcatch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        }
     }
 
-    static final int hash(Object key) {
-        int h;
-        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
-    }
 }
